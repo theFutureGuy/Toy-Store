516de39715fa5c7a12ae1e6308e8846413d37baf
theFutureGuy <Ashiq789git@gmail.com>
2022-07-07 19:53:37 +0530

    toystore

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">toy store</a>
+      </header>
+      <main>List of toys</main>
+    </div>
+  );
+}
+
+export default App;
